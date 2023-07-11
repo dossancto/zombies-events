435a9751d3e609a825355138167556fe655ddd9d
lu-css <dossantossilvalucas963@gmail.com>
2023-07-11 11:18:04 -0300

    [style] remove comment

--- src/repositories/twitch_streamers_repository.rb
+++ src/repositories/twitch_streamers_repository.rb
@@ -18,21 +18,3 @@ module TwitchStreamersRepository
     collection.insert_one(document)
   end
 end
-
-# client = DatabaseConfig.database_connection
-# # users = db[:users].find
-# # users.to_a.to_json
-
-# # Get a reference to the collection
-# collection = client[:your_collection_name]
-
-# # Define the document to insert
-# document = { field1: 'value1', field2: 'value2' }
-
-# # Insert the document
-# result = collection.insert_one(document)
-
-# # Check the result
-# return "Document inserted with ID: #{result.inserted_id}" if result.inserted_id
-
-# 'Failed to insert document'
