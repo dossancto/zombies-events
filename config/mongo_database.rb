ed9be8cc81c24558b7057755d931044d31806292
lu-css <dossantossilvalucas963@gmail.com>
2023-07-11 13:09:36 -0300

    change auth source

--- config/mongo_database.rb
+++ config/mongo_database.rb
@@ -10,7 +10,7 @@ module MongoDatabase
     database_url = case ENV['RACK_ENV']
                    when 'production'
                      # "mongodb://#{ENV['MONGO_USERNAME']}:#{ENV['MONGO_PASSWORD']}@#{ENV['MONGO_HOST']}:#{ENV['MONGO_PORT']}/#{database_name}_prod"
-                    "mongodb+srv://#{ENV['MONGO_USERNAME']}:#{ENV['MONGO_PASSWORD']}@aether.mkeuhzv.mongodb.net/#{database_name}?retryWrites=true&w=majority"
+                    "mongodb+srv://#{ENV['MONGO_USERNAME']}:#{ENV['MONGO_PASSWORD']}@aether.mkeuhzv.mongodb.net/#{database_name}?retryWrites=true&w=majority&authSource=admin"
                    else
                      "mongodb://root:example@127.0.0.1:27017/#{database_name}_dev?serverSelectionTimeoutMS=2000&authSource=admin"
                    end
