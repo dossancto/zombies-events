4e88fd0dc82fd39a350c049101bf1469455ffaac
lu-css <dossantossilvalucas963@gmail.com>
2023-07-11 14:01:22 -0300

    mongodb host

--- config/mongo_database.rb
+++ config/mongo_database.rb
@@ -9,8 +9,7 @@ module MongoDatabase
     database_name = 'zombies_events'
     database_url = case ENV['RACK_ENV']
                    when 'production'
-                     # "mongodb://#{ENV['MONGO_USERNAME']}:#{ENV['MONGO_PASSWORD']}@#{ENV['MONGO_HOST']}:#{ENV['MONGO_PORT']}/#{database_name}_prod"
-                    "mongodb+srv://#{ENV['MONGO_USERNAME']}:#{ENV['MONGO_PASSWORD']}@aether.mkeuhzv.mongodb.net/#{database_name}?retryWrites=true&w=majority&authSource=admin"
+                    "mongodb+srv://#{ENV['MONGO_USERNAME']}:#{ENV['MONGO_PASSWORD']}@#{ENV['MONGO_HOST']}/#{database_name}?retryWrites=true&w=majority&authSource=admin"
                    else
                      "mongodb://root:example@127.0.0.1:27017/#{database_name}_dev?serverSelectionTimeoutMS=2000&authSource=admin"
                    end
