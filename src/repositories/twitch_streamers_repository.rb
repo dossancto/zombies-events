652c1de4a7b70f405c922d9bd2d8a417f0d8599d
lu-css <dossantossilvalucas963@gmail.com>
2023-07-12 18:18:45 -0300

    [feat] reduce mongodb data

--- src/repositories/twitch_streamers_repository.rb
+++ src/repositories/twitch_streamers_repository.rb
@@ -8,13 +8,28 @@ module TwitchStreamersRepository
     viewer_count = lives.inject(0) { |sum, x| sum + x.viewer_count }
     count = lives.length
 
+    mongo_lives = lives.map do |live|
+      to_mongodb_live(live)
+    end
+
     document = {
-      lives: lives.as_json,
+      lives: mongo_lives,
       created_at: DateTime.now,
       total_views: viewer_count,
       lives_count: count
     }
 
     collection.insert_one(document)
+  end
+
+  def self.to_mongodb_live(live)
+    {
+      user_login: live.user_login,
+      title: live.title,
+      language: live.language,
+      started_at: live.started_at,
+      game_name: live.game_name,
+      viewer_count: live.viewer_count
+    }
   end
 end
